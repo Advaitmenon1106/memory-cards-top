2e3f56ffd825f4760aab798b974473fd90902779
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-12-16 03:11:44 +0530

    Modify main.jsx to temporarily include the image grabber jsx file to test it.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Image_Grabber } from './Image_Grabber'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Image_Grabber />
   </StrictMode>,
 )

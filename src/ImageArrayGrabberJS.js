19a8a8dd06d67e04a674faae18bc0386af117d1f
advaitmenon1106 <advaitmenon1106@gmail.com>
2025-01-01 01:44:08 +0530

    Make a function that returns a promise of the array of 80 pexels photos.

---
+++ src/ImageArrayGrabberJS.js
@@ -0,0 +1,21 @@
+import { createClient } from "pexels";
+
+export function ImageArrayGrabberJS(page_num = 1) {
+  const client = createClient(
+    "w93M0NVwaG2KkpiVWcIizUINC4lWLaUP9siQKz7dknMuIgR7lfgUrOoL"
+  );
+
+  const query = "tree";
+
+  const resultsPromise = client.photos.search({
+    query,
+    per_page: 80,
+    page: page_num,
+  });
+  return resultsPromise;
+
+  // const client = createClient('w93M0NVwaG2KkpiVWcIizUINC4lWLaUP9siQKz7dknMuIgR7lfgUrOoL');
+  // const query = 'tree';
+  // const searchResults = client.photos.search({ query, per_page: 80, page:page_num })
+  // return searchResults
+}
